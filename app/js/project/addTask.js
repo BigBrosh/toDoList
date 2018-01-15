a158eba3da065b0151a18d93d619770747e393d4
BigBrosh <30599468+BigBrosh@users.noreply.github.com>
2018-01-15 22:37:53 +0200

    added task handler

--- app/js/project/addTask.js
+++ app/js/project/addTask.js
@@ -1,17 +1,17 @@
 import {Task} from './task';
+import {TaskHandler} from './taskHandler';
 
 var addButton = document.getElementById('add_task'),
 	taskName = document.getElementById('task_name'),
 	taskDate = document.getElementById('task_date'),
 	taskDescription = document.getElementById('task_description'),
-	task_id = 0,
-	newTask = new Task(task_id, taskName, taskDate, taskDescription);
+	task_id = 0;
 
 
 addButton.onclick = function() {
-	newTask.name = taskName.value;
-	newTask.date = taskDate.value;
-	newTask.description = taskDescription.value;
+	var newTask = new Task(task_id, taskName.value, taskDate.value, taskDescription.value);
 	newTask.increaseId();
 	console.log(newTask);
+	TaskHandler.add(newTask);
+	console.log(TaskHandler.array);
 };
---
+++ app/js/project/taskHandler.js
@@ -0,0 +1,13 @@
+var taskArray = [];
+
+var handler = class {
+	constructor(array) {
+		this.array = array;
+	}
+
+	add(task) {
+		this.array.push(task);
+	}
+}
+
+export var TaskHandler = new handler(taskArray);
